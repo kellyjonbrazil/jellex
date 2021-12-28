822646bfb296c370c767b8256861fd01bd9e8a47
Kelly Brazil <kellyjonbrazil@gmail.com>
2021-12-28 08:48:24 -0800

    version bump

--- jellex/__init__.py
+++ jellex/__init__.py
@@ -1,7 +1,7 @@
 """jellex - TUI to query JSON at the command line with python syntax"""
 
 
-__version__ = '0.5.3'
+__version__ = '0.5.4'
 AUTHOR = 'Kelly Brazil'
 WEBSITE = 'https://github.com/kellyjonbrazil/jellex'
 COPYRIGHT = '© 2021 Kelly Brazil'
